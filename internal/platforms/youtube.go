f64c23934c35ee32f4d08ac43825f471660cc71e
rajpurohitnikki121-hue <rajpurohitnikki121@gmail.com>
2026-08-30 16:12:07 +0530

    Change error handling to check IsStatusFailure
    Signed-off-by: rajpurohitnikki121-hue <rajpurohitnikki121@gmail.com>

--- internal/platforms/youtube.go
+++ internal/platforms/youtube.go
@@ -485,7 +485,7 @@ func (p *YouTubePlatform) callInnerTube(endpoint string, body, result any) error
 		return fmt.Errorf("innertube request failed: %w", err)
 	}
 
-	if resp.IsError() {
+	if resp.IsStatusFailure() {
 		return fmt.Errorf("innertube error: %d", resp.StatusCode())
 	}
 
